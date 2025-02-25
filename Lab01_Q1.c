0914b6e0ffacb4b2fbf6ba83d3800c1312b218a8
krod7 <179400544+krod7@users.noreply.github.com>
2025-02-25 15:16:27 -0600

    Lab01_Q1.c

--- Lab01_Q1.c
+++ Lab01_Q1.c
@@ -1,59 +1,43 @@
-// Leetcode 203: Remove Linked List Elements
-/*
-Given the head of a linked list and an integer val,
-remove all the nodes of the linked list that has
-Node.val == val, and return the new head.
-
-Example 1:
-Input: head = [1,2,6,3,4,5,6], val = 6
-Output: [1,2,3,4,5]
-
-Example 2:
-Input: head = [], val = 1
-Output: []
-
-Example 3:
-Input: head = [7,7,7,7], val = 7
-Output: []
-*/
-
-/*
+/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     struct ListNode *next;
  * };
- */
+*/
+
+/**
+ * Removes all nodes with the given value from the linked list
+ * struct ListNode is already defined
+*/
 
 #include <stdlib.h>
-struct ListNode *removeElements(struct ListNode *head, int val)
-{
+struct ListNode* removeElements(struct ListNode* head, int val) {
     // Create a dummy node to handle edge cases
-    struct ListNode *dummy = (struct ListNode *)malloc(sizeof(struct ListNode));
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
     // The dummy updates the head
     dummy->next = head;
-
+    
     // Pointer to traverse the list
-    struct ListNode *current = dummy;
+    // Initialize pointer to the dummy node
+    struct ListNode* current = dummy;
 
-    while (current->next)
-    {
-        if (current->next->val == val)
-        {
-            // Store node to be deleted
-            struct ListNode *temp = current->next;
+    // while loop to iterate through all nodes in the linked list
+    while (current->next) {
+        // Check if nodes in the list = val
+        if (current->next->val == val) {
+            // If yes: Store node to be deleted
+            struct ListNode* temp = current->next;
             // Remove the node
             current->next = current->next->next;
             // Free the memory
             free(temp);
-        }
-        else
-        {
-            // Move to the next node
+        } else {
+            // If no: Move to the next node
             current = current->next;
         }
     }
-
+    
     // Update head
     head = dummy->next;
     // Free dummy node

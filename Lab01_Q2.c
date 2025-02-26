5c2592d3123d08f2f2551d8b5a84bb0151f24680
krod7 <179400544+krod7@users.noreply.github.com>
2025-02-25 21:49:35 -0600

    Additional Comments Lab01_Q2.c

--- Lab01_Q2.c
+++ Lab01_Q2.c
@@ -1,54 +1,44 @@
-// Leetcode 206: Reverse Linked List
-/*
-Given the head of a singly linked list, reverse the list,
-and return the reversed list.
-
-Example 1:
-Input: head = [1,2,3,4,5]
-Output: [5,4,3,2,1]
-
-Example 2:
-Input: head = [1,2]
-Output: [2,1]
-
-Example 3:
-Input: head = []
-Output: []
-*/
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     struct ListNode *next;
  * };
- */
+*/
 
 #include <stdlib.h>
 
 /**
  * Reverses a singly linked list.
  * struct ListNode is already defined.
- */
+*/
 
-struct ListNode *reverseList(struct ListNode *head)
-{
-    struct ListNode *prev = NULL;
-    struct ListNode *current = head;
-    struct ListNode *next = NULL;
+struct ListNode* reverseList(struct ListNode* head) {
+    // Initialize 3 ptrs
+    // Track previous nodes, initialized NULL b/c the new last node should be NULL
+    struct ListNode* prev = NULL;
+    // Track the node being processed, initialized head b/c it is the beginning of the list
+    struct ListNode* current = head;
+    // Store the next node temporarily to prevent losing track of the order of the list
+    struct ListNode* next = NULL;
 
-    while (current != NULL)
-    {
+    // while loop to iterate through all nodes in list
+    while (current != NULL) {
         // Store the next node
+        // Do not modify it or else
         next = current->next;
         // Reverse the link
+        // Changes the direction of the links the opposite way
+        // Instead of pointing forward, point backward to the previous node
         current->next = prev;
         // Move prev to the current node
+        // Keep track of the reversed parts of the list
         prev = current;
         // Move current to the next node
+        // Once done processing a node, continue to the next one
         current = next;
     }
-
+    
     // New head of the reversed list
     return prev;
 }
